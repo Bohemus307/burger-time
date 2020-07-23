3a321c4e6abbc0374ab03d8cd34ef2cc2ad82cca
joshua oxner <bohemus@hotmail.com>
2020-07-23 09:06:16 -0600

    buttons working now to add modal

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,11 @@ import classes from './BuildControl.css';
 const BuildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <button className={classes.Less}>LESS</button>
-    <button className={classes.More} onClick={props.added}>MORE</button>
+    <button className={classes.Less} 
+    onClick={props.removed} 
+    disabled={props.disabled}>LESS</button>
+    <button className={classes.More} 
+    onClick={props.added}>MORE</button>
   </div>
 );
 
