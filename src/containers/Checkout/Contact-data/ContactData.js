f28abe435e9b548cd2a15b5eaa3410c532274c63
joshua oxner <bohemus@hotmail.com>
2020-10-05 15:49:10 -0600

    adding rerouting to builder finish

--- src/containers/Checkout/Contact-data/ContactData.js
+++ src/containers/Checkout/Contact-data/ContactData.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
+import axios from '../../../axios-orders';
+
 import classes from './ContactData.css';
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
   state = {
@@ -10,19 +13,56 @@ class ContactData extends Component {
     address: {
       street:'',
       postalCode: ''
-    }
+    },
+    loading: false,
   }
+
+  orderHandler = (event) => {
+    event.preventDefault()
+    // request 
+    this.setState({ loading: true })
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: 'j money',
+        address: {
+          street: 'setreet street',
+          zipcode: '888888',
+          country: 'usa'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+    .then(response => {
+      this.setState({ loading:false });
+      this.props.history.push('/');
+
+    })
+    .catch((error) => {
+      this.setState({ loading:false });
+    })
+  }
+
   render() {
+    let form = (
+      <form>
+        <input className={classes.Input} type='text' name='name' placeholder='your Name' />
+        <input className={classes.Input} type='text' name='email' placeholder='your Mail' />
+        <input className={classes.Input} type='text' name='street' placeholder='Street' />
+        <input className={classes.Input} type='text' name='postal' placeholder='Postal Code' />
+        <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
+      </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
     return (
       <div className={classes.ContactData}>
         <h4>Enter your contact data</h4>
-        <form>
-          <input className={classes.Input} type='text' name='name' placeholder='your Name' />
-          <input className={classes.Input} type='text' name='email' placeholder='your Mail' />
-          <input className={classes.Input} type='text' name='street' placeholder='Street' />
-          <input className={classes.Input} type='text' name='postal' placeholder='Postal Code' />
-          <Button btnType='Success'>ORDER</Button>
-        </form>
+        {form}
       </div>
     );
   }
