af3f136d4aba76726a1fd5fb40ff282e3f62ac9a
joshua oxner <bohemus@hotmail.com>
2020-10-06 07:55:07 -0600

    nav links updated

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 import classes from './navigationitem.css'
 
 const NavigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a href={props.link} 
-    className={ props.active ? classes.active : null}>{props.children}</a>
+    <NavLink activeClassName={classes.active} to={props.link} exact={props.exact}>{props.children}</ NavLink>
   </li>
 );
 
