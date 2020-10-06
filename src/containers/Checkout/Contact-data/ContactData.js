af24f93bd381b7e08429ffdadb6448da43268ebb
joshua oxner <bohemus@hotmail.com>
2020-10-06 10:18:20 -0600

    adding inout component to replace inout tags

--- src/containers/Checkout/Contact-data/ContactData.js
+++ src/containers/Checkout/Contact-data/ContactData.js
@@ -5,6 +5,7 @@ import axios from '../../../axios-orders';
 import classes from './ContactData.css';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
@@ -49,10 +50,10 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input className={classes.Input} type='text' name='name' placeholder='your Name' />
-        <input className={classes.Input} type='text' name='email' placeholder='your Mail' />
-        <input className={classes.Input} type='text' name='street' placeholder='Street' />
-        <input className={classes.Input} type='text' name='postal' placeholder='Postal Code' />
+        <Input inputType="input" className={classes.Input} type='text' name='name' placeholder='your Name' />
+        <Input inputType="input" className={classes.Input} type='text' name='email' placeholder='your Mail' />
+        <Input inputType="input" className={classes.Input} type='text' name='street' placeholder='Street' />
+        <Input inputType="input" className={classes.Input} type='text' name='postal' placeholder='Postal Code' />
         <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
       </form>
     );
