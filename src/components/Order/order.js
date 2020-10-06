a680676adfaa114469a7d2d1532f77092ab7a809
joshua oxner <bohemus@hotmail.com>
2020-10-06 08:22:21 -0600

    ng orders and rendering them in orders page

--- src/components/Order/order.js
+++ src/components/Order/order.js
@@ -2,11 +2,40 @@ import React from 'react';
 
 import classes from './order.css';
 
-const order = (props) => (
-  <div className={classes.order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>USD 5.44</strong></p>
-  </div>
-);
+const order = (props) => {
+  const ingredients = [];
+
+  for (let ingredientName in props.ingredients) {
+    ingredients.push(
+      {
+        name: ingredientName,
+        amount: props.ingredients[ingredientName]
+      }
+    );
+  }
+
+  const ingredientOuput = ingredients.map(ig => {
+    return (
+      <span 
+        key={ig.name} 
+        style={{ 
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px"
+        }}>
+          {ig.name} ({ig.amount})
+      </span>
+    )
+  })
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients:  {ingredientOuput}</p>
+      <p>Price: <strong>USD {props.price}</strong></p>
+    </div>
+  );
+};
 
 export default order;
